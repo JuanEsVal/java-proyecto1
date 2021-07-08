21fe8332012d64b64f0ba3f4c6032db12dca71bd
JuanEsVal <juanes.valenciag@gmail.com>
2021-07-08 12:15:33 -0500

    Comentado, Formateado y Organizado

--- clase02/src/main/java/co/edu/utp/misiontic2022/c2/App.java
+++ clase02/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class App
 {
     public static void main( String[] args )
-    {
+    {        
+        
+        
         Scanner sc = new Scanner(System.in);
         
+        //Variables para almacenar datos iniciales
         String ficha;
         String color;
         int posInicial;
         int posFinal;
 
+        //Solicitando datos desde consola
         System.out.print("\nEscriba un nombre para la ficha: ");  
         ficha = sc.nextLine();
         System.out.print("Escriba un color para la ficha: ");  
@@ -20,30 +24,34 @@ public class App
         System.out.print("Escriba la posicion Inicial de la ficha: ");        
         posInicial = sc.nextInt();
 
+        //Creando un Objeto de tipo Ficha
         Ficha objFicha1 = new Ficha(ficha, color, posInicial);
+        
+        //Imprimiendo instancia de la ficha
         System.out.println("--------------------------------");        
         System.out.println("Datos de la ficha: ");
         System.out.println("--------------------------------");        
         System.out.println( objFicha1 );  // --> objFicha1.toString()
         System.out.println("--------------------------------");        
 
+        //Solicitando nueva posicion para la ficha, hacemos el movimiento y mostramos el desplazamiento
         System.out.print("\nAhora...  Escriba la posicion hacia donde desea mover esta ficha (" + objFicha1.getNombre() + " " + objFicha1.getColor() + ") :");        
         posFinal = sc.nextInt();        
         System.out.println( objFicha1.movimiento(objFicha1.getNombre(), objFicha1.getPosicion(), posFinal) );
         System.out.println( objFicha1.desplazamieto(objFicha1.getNombre(), objFicha1.getColor() , posFinal) );
         System.out.println("--------------------------------");        
         
+        //Seteamos la nueva posicion de la ficha
         objFicha1.setPosicion(posFinal);
+        
+        //Solicitando de nuevo una nueva posicion para la ficha, hacemos el nuevo movimiento y mostramos el desplazamiento
         System.out.print("\nAhora...  Escriba la nueva posicion hacia donde desea mover esta ficha (" + objFicha1.getNombre() + " " + objFicha1.getColor() + ") :");        
         posFinal = sc.nextInt();
-        System.out.println("--------------------------------");        
-        
+        System.out.println("--------------------------------");                
         System.out.println( objFicha1.movimiento(objFicha1.getNombre(), objFicha1.getPosicion(), posFinal) );
         System.out.println( objFicha1.desplazamieto(objFicha1.getNombre(), objFicha1.getColor() , posFinal) );
         
-
-        System.out.println();        
-        System.out.println();
-        sc.close();
+        System.out.println("\n");
+        sc.close(); // Cerrando el objeto Scanner
     }
 } 
